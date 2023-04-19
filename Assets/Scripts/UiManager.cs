b59ecfeaf44c2326ff31848689c9be9ddad7b8be
AlebiGit <122869664+AlebiGit@users.noreply.github.com>
2023-04-19 16:30:23 +0200

    Scriblo

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -10,15 +10,21 @@ public class UiManager : MonoBehaviour
     public static UiManager instance;
 
     [SerializeField] Canvas canvas;
+    private CanvasScaler canvasScaler;
 
     [SerializeField] RectTransform fullnessBar;
     [SerializeField] RectTransform loveBar;
     [SerializeField] RectTransform hygeneBar;
 
-    public bool updateBars;
+    [HideInInspector]public bool updateBars;
 
-    [SerializeField] RectTransform crossWordWindow;
-    [SerializeField] RectTransform crosswordSpritemaskSize;
+    [SerializeField] RectTransform crossWordPanel;
+    private Vector3 crossWordWindow_onScreenPosition;
+    private Vector3 crossWordWindow_offScreenPosition;
+    [SerializeField] RectTransform fullTastieraPanel;
+    private Vector3 tastiera_onScreenPosition;
+    private Vector3 tastiera_offScreenPosition;
+    [SerializeField] RectTransform tastieraBackground;
     [SerializeField] SpriteMask spriteMask;
     
     [SerializeField] GameObject activeShopTab;
@@ -38,8 +44,9 @@ public class UiManager : MonoBehaviour
     }
     void Start()
     {
+        canvasScaler = canvas.GetComponent<CanvasScaler>();
         UiResizer();
-        
+        GetAnimationPositions();
     }
 
     private void LateUpdate()
@@ -52,21 +59,43 @@ public class UiManager : MonoBehaviour
     {
         rectTransform.rect.Set(rectTransform.rect.x, rectTransform.rect.y, rectTransform.rect.width * values.x, rectTransform.rect.height * values.y);
     }
-
     private void UiResizer()
     {
         RectTransform canvasRectTransform = canvas.GetComponent<RectTransform>();
-        Vector2 referenceRes = canvas.GetComponent<CanvasScaler>().referenceResolution;
+        Vector2 referenceRes = canvasScaler.referenceResolution;
         Vector2 res = new Vector2(canvasRectTransform.rect.width, canvasRectTransform.rect.height);
         Vector2 scaleMult = new Vector2(res.x / referenceRes.x, res.y / referenceRes.y);
-        Debug.Log("REFERENCE RES:" + referenceRes +  "   RES:" + res.x + "," + res.y + "    SCALE MULT:" + scaleMult + "    CONVAS POS:" + canvas.transform.position + "    PANEL POS:" + crossWordWindow.transform.position);
+        MultRectTransformWidthAndHeight(crossWordPanel, scaleMult);
 
-        MultRectTransformWidthAndHeight(crossWordWindow, scaleMult);
-        //crossWordWindow.position = canvas.transform.position + Vector3.up * canvasRectTransform.rect.height;
-        crossWordWindow.transform.position = new Vector3(canvas.transform.position.x, canvasRectTransform.offsetMax.y, canvas.transform.position.z); ;
-        
-        spriteMask.transform.position = crosswordSpritemaskSize.transform.position;
-        spriteMask.transform.localScale = new Vector3(crosswordSpritemaskSize.rect.width, crosswordSpritemaskSize.rect.height, 1);
+        spriteMask.transform.localScale = new Vector3(tastieraBackground.rect.width, tastieraBackground.rect.height, 1);
+        spriteMask.transform.position = fullTastieraPanel.transform.position + new Vector3(0, tastieraBackground.rect.height * canvas.transform.lossyScale.y / 2, 0);
+    }
+
+    private void GetAnimationPositions()
+    {
+        Vector3 heihtContainer; //la x non è corretta. Serve solo la y
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(crossWordPanel, new Vector2(0, Screen.currentResolution.height), Camera.main, out heihtContainer);
+        crossWordWindow_offScreenPosition = new Vector3(canvas.transform.position.x, heihtContainer.y, canvas.transform.position.z);
+        crossWordWindow_onScreenPosition = crossWordWindow_offScreenPosition - new Vector3(0, crossWordPanel.rect.height * canvas.transform.lossyScale.y, 0);
+        crossWordPanel.transform.position = crossWordWindow_offScreenPosition;
+
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(fullTastieraPanel, new Vector2(0, 0), Camera.main, out heihtContainer);
+        tastiera_onScreenPosition = new Vector3(canvas.transform.position.x, heihtContainer.y, canvas.transform.position.z);
+        tastiera_offScreenPosition = tastiera_onScreenPosition - new Vector3(0, fullTastieraPanel.rect.height * canvas.transform.lossyScale.y, 0);
+        fullTastieraPanel.transform.position = tastiera_offScreenPosition;
+    }
+    public void AnimateCrosswordPanels(bool onScreen)
+    {
+        if (onScreen)
+        {
+            crossWordPanel.transform.position = crossWordWindow_onScreenPosition;
+            fullTastieraPanel.transform.position = tastiera_onScreenPosition;
+        }
+        else
+        {
+            crossWordPanel.transform.position = crossWordWindow_offScreenPosition;
+            fullTastieraPanel.transform.position = tastiera_offScreenPosition;
+        }
     }
 
     private void UpdateBars()
@@ -78,6 +107,7 @@ public class UiManager : MonoBehaviour
             updateBars = true;
         }
     }
+
     public void SelectShopTab(GameObject tab)
     {
         if(activeShopTab) activeShopTab.SetActive(false);
